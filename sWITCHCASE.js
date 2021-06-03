6a2f2b932f4d83ec06f1b434b7612e92e50ad5d3
dikoleshubh <dikole.shubh@gmail.com>
2021-06-03 16:05:59 +0530

    SWITCH CASES

--- sWITCHCASE.js
+++ sWITCHCASE.js
@@ -34,3 +34,30 @@ switch(number)
             console.log("Nine");
             break;
 }
+const prompt = require('prompt-sync')();
+//Uc 2. Read a number and display the day
+let number = Number(prompt('Enter the number : '));
+switch(number)
+{
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;        
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+}
